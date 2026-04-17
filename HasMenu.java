02b8d7a6ae9f8c069dcd0f60d0300f1217c3b6a9
janblocker2003 <janblocker2003@gmail.com>
2026-04-17 16:08:50 +0000

    Added HasMenu and NoteCard

---
+++ HasMenu.java
@@ -0,0 +1,9 @@
+//Has Menu Interface
+
+public interface HasMenu {
+	public String menu();
+	public void start();
+	public static long serialVersionUID = 1L;
+
+} // end interface
+
---
+++ NoteCard.java
@@ -0,0 +1,55 @@
+//Note Card class
+
+import java.util.*;
+import java.io.*;
+
+public class NoteCard implements HasMenu, Serializable {
+	
+	String word;
+	String definition;
+	int correct = 0;
+	int incorrect = 0;
+
+	public NoteCard() {
+		this.word = "";
+		this.definition = "";
+	} // end note card no paramater constructor
+	
+	public NoteCard(String newWord, String newDefinition) {
+		this.word = newWord;
+		this.definition = newDefinition;
+	} // end constructor with paramters
+	
+	public String getWord() {
+		return this.word;
+	} // end getWord
+	
+	public void setWord(newWord) {
+		this.word = newWord;
+	} // end setWord
+	
+	public String getDefinition() {
+                return this.definition;
+        } // end getDefinition
+
+        public void setDefinition(newDefinition) {
+                this.definition = newDefinition;
+        } // end setDefinition
+	
+	public int getCorrect() {
+		return this.correct;
+	} // end getCorrect
+	
+	public void setCorrect(int newCorrect) {
+		this.correct = newCorrect;
+	} // end setCorrect
+	
+	public int getIncorrect() {
+                return this.incorrect;
+        } // end getIncorrect
+
+        public void setIncorrect(int newIncorrect) {
+                this.incorrect = newIncorrect;
+        } // end setIncorrect
+
+} // end NoteCard class
