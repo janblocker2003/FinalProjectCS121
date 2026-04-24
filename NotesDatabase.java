8c5a6d018049e28baca68aee5fab79c9533efbfa
janblocker2003 <janblocker2003@gmail.com>
2026-04-24 15:52:23 +0000

    added most of notes database

--- NotesDatabase.java
+++ NotesDatabase.java
@@ -9,16 +9,113 @@ public class NotesDatabase implements HasMenu, Serializable {
 	String noteCardString;
 	NoteCardList noteCards = new NoteCardList();
 
+	String BOLD = "\u001B[1m";
+        String RESET = "\u001B[0m";
+
 	public NotesDataBase() {
 		this.name = "";
 		this.noteCardString = "";
 	} // end NotesDataBase constructor with no parameters
 	
-	public loadSampleNote() {
+	public void loadSampleNote() {
 		this.name = "Sample";
 		this.noteCardString = "Elephant,an animal,Turkey,flying animal";
+		// loadSampleCustomer within Bank.java shows good example of way to do this if this doesn't work well
 	} // end Load Sample Note
 	
+	public void loadNotes() {
+		try {
+			FileInputStream fi = new FileInputStream("NoteCards.dat");
+			ObjectInputStream obIn = new ObjectInputStream(fi);
+			noteCards = (NoteCardList)obIn.readObject();
+			obIn.close();
+			fi.close();
+		} // end try
+		catch (Exception e) {
+			System.out.println(e.Message());
+		} // end catch
+	} // end load notes
+	
+	public void saveNotes() {
+		try {
+			FileOutputStream fo = new FileOutputStream("NoteCards.dat");
+			ObjectOutputStream obOut = new ObjectOutputStream(fo);
+			obOut.writeObject(noteCards);
+			obOut.close();
+			fo.close();
+		} // end try
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		} // end catch
+	} // save notes
+	
+	public String menu() {
+		Scanner input = new Scanner(System.in);
+		System.out.println();
+		System.out.println(BOLD + "NoteCard Menu " + RESET);
+		System.out.println("______________________________");
+		System.out.println();
+		System.out.println("0) Exit "+ this.name + " database");
+		System.out.println("1) Progress Report ");
+		System.out.println("2) Add a note card to " + this.name);
+		System.out.println("3) Delete a note card from " + this.name);
+		System.out.println("4) Resort note cards in " + this.name);
+		System.out.println("______________________________");
+		System.out.println();
+		System.out.print("Your response: ");
+		String response = input.nextLine();
+
+		return response;
+	} // end menu
+	
+	public void start() {
+		boolean keepGoing = true;
+		while (keepGoing) {
+			String response = menu();
+			if (response.equals("0")) {
+				keepGoing = false;
+			} // end if
+			else if (response.equals("1")) {
+				System.out.println();
+				System.out.println(BOLD + "Progress Report for " + this.name + RESET);
+				System.out.println("______________________________");
+				System.out.println();
+				progressReport();
+				} // end if
+			} // end else if
+			else if (response.equals("2")) {
+				System.out.println();
+				System.out.println(BOLD + "Add a note card to " + this.name + RESET);
+				System.out.println("______________________________");
+				System.out.println();
+				addNoteCard();
+			} // end else if
+			else if (response.equals("3")) {
+                                System.out.println();
+                                System.out.println(BOLD + "Delete a note card from " + this.name + RESET);
+                                System.out.println("______________________________");
+                                System.out.println();
+                                deleteNoteCard();
+                        } // end else if
+			else if (response.equals("4")) {
+                                System.out.println();
+                                System.out.println(BOLD + "Resort notecards in " + this.name + RESET);
+                                System.out.println("______________________________");
+                                System.out.println();
+                                sortNotes();
+                        } // end else if
+			else {
+				System.out.println("Please enter 0, 1, 2, 3, or 4 ");
+			} // end else
+		} // end while 
+	} // end start
+	
+	public void progressReport() {
+		for (NoteCard noteCard: noteCards) {
+			System.out.println(noteCard.getReport());
+		} // end for
+	} // end progress report
+
 } // end Notes DataBase
 
 class NoteCardList extends ArrayList<NoteCard> {};
